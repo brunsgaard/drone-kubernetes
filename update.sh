41aab26296d7d31961750f262addd02d1586b9ca
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2017-06-29 17:11:48 +0200

    Drone experiments

--- update.sh
+++ update.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
-if [ -z ${PLUGIN_NAMESPACE} ]; then
-  PLUGIN_NAMESPACE="default"
-fi
-
-if [ ! -z ${PLUGIN_KUBERNETES_TOKEN} ]; then
-  KUBERNETES_TOKEN=$PLUGIN_KUBERNETES_TOKEN
-fi
 
 if [ ! -z ${PLUGIN_KUBERNETES_SERVER} ]; then
   KUBERNETES_SERVER=$PLUGIN_KUBERNETES_SERVER
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
-  KUBERNETES_CERT=${PLUGIN_KUBERNETES_CERT}
+if [ ! -z ${PLUGIN_KUBERNETES_USERNAME} ]; then
+  KUBERNETES_USERNAME=$PLUGIN_KUBERNETES_USERNAME
 fi
 
-kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
-if [ ! -z ${KUBERNETES_CERT} ]; then
-  echo ${KUBERNETES_CERT} | base64 -d > ca.crt
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
-else
-  echo "WARNING: Using insecure connection to cluster"
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+if [ ! -z ${PLUGIN_KUBERNETES_PASSWORD} ]; then
+  KUBERNETES_PASSWORD=$PLUGIN_KUBERNETES_PASSWORD
 fi
 
+
+# Building the config and setting the context
+kubectl config set-credentials default --username=${KUBERNETES_USERNAME} --password=${KUBERNETES_PASSWORD}
+kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
 
+kubectl get pods
+
 # kubectl version
-IFS=',' read -r -a DEPLOYMENTS <<< "${PLUGIN_DEPLOYMENT}"
-IFS=',' read -r -a CONTAINERS <<< "${PLUGIN_CONTAINER}"
-for DEPLOY in ${DEPLOYMENTS[@]}; do
-  echo Deploying to $KUBERNETES_SERVER
-  for CONTAINER in ${CONTAINERS[@]}; do
-    kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} \
-      ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG} --record
-  done
-done
+# IFS=',' read -r -a DEPLOYMENTS <<< "${PLUGIN_DEPLOYMENT}"
+# IFS=',' read -r -a CONTAINERS <<< "${PLUGIN_CONTAINER}"
+# for DEPLOY in ${DEPLOYMENTS[@]}; do
+#   echo Deploying to $KUBERNETES_SERVER
+#   for CONTAINER in ${CONTAINERS[@]}; do
+#     kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} \
+#       ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG} --record
+#   done
+# done
